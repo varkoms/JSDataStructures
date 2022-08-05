e9aa3519999910d05e20e2ed5206d8b58aedadbd
varkoms <cmendozasoberanis@gmail.com>
2022-08-05 18:55:52 -0500

    Implementando el metodo Get

--- hashtable.js
+++ hashtable.js
@@ -1,12 +1,12 @@
 class HashTable {
-  constructor(size){
+  constructor(size) {
     this.data = new Array(size);
   }
 
   hashMethod(key) {
     let hash = 0;
     for (let i = 0; i < key.length; i++) {
-      hash = (hash + key.charCodeAt(i) * i) % this.data.length; 
+      hash = (hash + key.charCodeAt(i) * i) % this.data.length;
     }
 
     return hash;
@@ -14,18 +14,62 @@ class HashTable {
 
   set(key, value) {
     const address = this.hashMethod(key);
-    
-    if(!this.data[address]) {
+
+    if (!this.data[address]) {
       this.data[address] = [];
     }
-    
+
     this.data[address].push([key, value]);
 
     return this.data;
   }
+
+  get(key) {
+    const address = this.hashMethod(key);
+    const currentBucket = this.data[address];
+
+    if (currentBucket) {
+      for (let i = 0; i < currentBucket.length; i++) {
+        if (currentBucket[i][0] === key) {
+          return currentBucket[i][1];
+        }
+      }
+    }
+
+    return undefined;
+  }
+
+  getAllKeys() {
+    const keys = [];
+    for (let i = 0; i < this.data.length; i++) {
+      if (this.data[i]) {
+        for (let j = 0; j < this.data[i].length; j++) {
+          keys.push(this.data[i][j][0]);
+        }
+      }
+    }
+    return keys;
+  }
+
+  remove(key) {
+    const address = this.hashMethod(key);
+    const currentBucket = this.data[address];
+    if (currentBucket) {
+      for (let i = 0; i < currentBucket.length; i++) {
+        if (currentBucket[i][0] === key) {
+          const deletedValue = this.data[address][i];
+          this.data[address].splice(i, 1);
+          return deletedValue;
+        }
+      }
+    }
+    return undefined;
+  }
 }
 
 const myHashTable = new HashTable(20);
+
+// Set hashtable
 myHashTable.set("Cesar", 1989);
 myHashTable.set("Martin", 1991);
 myHashTable.set("Carlos", 1987);
@@ -38,3 +82,15 @@ myHashTable.set("Victor", 1985);
 myHashTable.set("Alejandra", 2001);
 
 console.log(myHashTable.data);
+
+// Get elements
+console.log(myHashTable.get("Alejandra"));
+
+// Get All Keys
+console.log(myHashTable.getAllKeys());
+
+// Delete
+console.log(myHashTable.remove("Oscar"));
+
+// Get All Keys
+console.log(myHashTable.getAllKeys());
